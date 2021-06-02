d179e80ce1169fa0129dafc8e7df7b0fb0775f4a
Aidan Evans <38838579+nsnave@users.noreply.github.com>
2021-06-02 01:17:25 -0400

    updated print functions appropriately

--- mclang/main.ml
+++ mclang/main.ml
@@ -30,16 +30,85 @@ type cmd =
 
 type prog = prep list * cmd list
 
+
+(**********************************************************************************
+ *                                                                                *
+ *                                Utility Functions                               *
+ *                                                                                *
+ **********************************************************************************)
+
+(*
+ *  Checks whether a program is well formed, i.e., whether it satisfies
+ *  the following conditions:
+ *    (D0) No command depends on an outcome not yet measured.
+ *    (D1) No command acts on a qubit already measured.
+ *    (D2) No command acts on a qubit not yet prepared, unless it is an input qubit.
+ *    (D3) A qubit i is measured if and only if i is not an output.
+ *)(*
+let well_formed (p : prog) : bool = (
+  
+);;*)
+
+(*
+ *  Converts numbers to a strings
+ *)
+let to_string = Int.to_string;;
+let float_to_string x = Printf.sprintf "%.5f" x;;
+
+(*
+ *  Converts a preperation to a friendly, human-readable string.
+ *)
+let prep_to_string p = (
+  match p with
+  | Init (qubit, base_angle) -> 
+    "Init[" ^ to_string qubit ^ ", " ^ float_to_string base_angle ^ "]"
+  | Init0 (qubit) -> 
+    "Init0[" ^ to_string qubit ^ "]"
+  | Init1 (qubit) ->
+    "Init1[" ^ to_string qubit ^ "]"
+  | InitPlus (qubit) ->
+    "Init+[" ^ to_string qubit ^ "]"
+  | InitMinus (qubit) ->
+    "Init-[" ^ to_string qubit ^ "]"
+  | InitNonInput (qubits) ->
+    "InitNonInput[" ^ (String.concat ", " (List.map to_string qubits)) ^ "]"
+);;
+  
+(*
+ *  Converts a command to a friendly, human-readable string.
+ *)
+let cmd_to_string c = (
+  match c with 
   | Entangle (left, right) ->
     "E[" ^ to_string left ^ ", " ^ to_string right ^ "]"
-  | XCorrect (value) ->
-    "X[" ^ to_string value ^ "]"
-  | ZCorrect (value) ->
-    "Z[" ^ to_string value ^ "]"
-  | Value v -> v;;
+  | Measure (qubit, angle, parity1, parity2) ->
+    "M[" ^ String.concat ", " [to_string qubit; float_to_string angle; to_string parity1; to_string parity2] ^ "]"
+  | XCorrect (qubit, signal) ->
+    "X[" ^ to_string qubit ^ ", " ^ to_string signal ^ "]"
+  | ZCorrect (qubit, signal) ->
+    "Z[" ^ to_string qubit ^ ", " ^ to_string signal ^ "]"
+);;
 
-let print_expr e = 
-  print_endline (to_string e);;
+(*
+ *  Prints out the program in a friendly, human-readable format.
+ *)
+let print_prog ((preps, cmds) : prog) = (
+  List.iter (fun x -> print_endline(prep_to_string(x))) preps;
+  List.iter (fun x -> print_endline(cmd_to_string(x))) cmds
+);;
 
 
-print_expr (XCorrect ((Value "1")));;
+(**********************************************************************************
+ *                                                                                *
+ *                              Evaluation Functions                              *
+ *                                                                                *
+ **********************************************************************************)
+(*
+(* Randomized *)
+let eval ((preps, cmds) : prog) : bool list = (
+  
+);;
+*)
+
+print_prog ([Init(1, 0.375324); InitNonInput([2; 3])], [Entangle(1, 2)]);;
+
