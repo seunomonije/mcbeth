8ea83b6eaa1a06eb57859bee87dedc86bb4b2201
Aidan Evans <38838579+nsnave@users.noreply.github.com>
2021-06-04 02:40:41 -0400

    created utility library to help with complex numbers

---
+++ mclang/lib/complexenv/cenv.ml
@@ -0,0 +1,43 @@
+(**
+  * Adds helper functions for dealing with complex numbers and
+  * redefined arithmetic operators accordingly.
+  * 
+  * Uses the standard library Complex module.
+  *
+  *)
+
+open Complex;;
+
+(**
+  * Creates a complex number.
+  * val c : float -> float -> Complex.t
+  *)
+let c re im : t = {
+  re = re;
+  im = im;
+};;
+
+(**
+  * Converts a complex number to a string.
+  *)
+let cstr n = (
+  let ftos x = Printf.sprintf "%.2f" x in
+  (ftos n.re) ^ "+" ^ (ftos n.im) ^ "i"
+);;
+
+(**
+  * Redefines arithmetic operators.
+  *)
+let ( + )   = add;; (* Addition *)
+let ( - )   = sub;; (* Subtraction *)
+let ( ~- )  = neg;; (* Unary Negation *)
+let ( * )   = mul;; (* Multiplication *)
+let ( / )   = div;; (* Division *)
+
+(**
+  * Creates new operator for quick exponentiation.
+  * 
+  * x ** y = Complex.pow x y
+  * I.e., x to the power of y 
+  *)
+let ( ** )  = pow;; (* Exponentiation *)
