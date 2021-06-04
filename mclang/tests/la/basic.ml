178a59596bdaa4995611bc661c5d1de5fde11965
Aidan Evans <38838579+nsnave@users.noreply.github.com>
2021-06-04 01:21:47 -0400

    added basic linear algebra test using lacaml

--- mclang/tests/la/basic.ml
+++ mclang/tests/la/basic.ml
@@ -1,2 +1,24 @@
 
-print_endline("foo");;
+open Format;;
+
+open Lacaml.D;;
+open Lacaml.Io;;
+
+let () = (
+  let a =
+    Mat.of_array
+      [|
+        [| 2.; 3. |];
+        [| 1.; -5. |];
+      |] 
+  in
+  let b =
+    Mat.of_array
+      [|
+        [| 4.; 3.; 6. |];
+        [| 1.; -2.; 3. |];
+      |] 
+  in
+  printf "a = @[%a@]@\n@\n" pp_fmat a;
+  printf "b = @[%a@]@\n@\n" pp_fmat b
+)
