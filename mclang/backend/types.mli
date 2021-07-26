c6627003558566664f3c94bd9a1999540ed056f2
Aidan Evans <38838579+nsnave@users.noreply.github.com>
2021-07-25 20:53:29 -0400

    rewrote types

--- mclang/backend/types.mli
+++ mclang/backend/types.mli
@@ -1,18 +1,16 @@
 
 type qubit = int
 
-type prep = 
+type cmd =
     Init of qubit * float
   | Init0 of qubit
   | Init1 of qubit
   | InitPlus of qubit
   | InitMinus of qubit
   | InitNonInput of qubit list
-
-type cmd =
-    Entangle of qubit * qubit
+  | Entangle of qubit * qubit
   | Measure of qubit * float * qubit list * qubit list
   | XCorrect of qubit * qubit list
   | ZCorrect of qubit * qubit list
 
-type prog = prep list * cmd list
+type prog = cmd list
