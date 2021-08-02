927a43c1b65758fc43c56411f4efb61ec6c2da8c
Aidan Evans <38838579+nsnave@users.noreply.github.com>
2021-08-01 21:41:01 -0400

    testing

--- mclang/tests/run/rand_eval.ml
+++ mclang/tests/run/rand_eval.ml
@@ -4,5 +4,5 @@ open Backend.Run;;
 open Lacamlext;;
 open Lacaml.Z;;
 
-let () = List.iter (fun p -> Vec.print (rand_eval p)) Programs.programs
+let () = List.iter (fun p -> Mat.print (rand_eval p)) Programs.programs
 
