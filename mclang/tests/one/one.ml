904fa1ea099d348fb7be46c73c9d0e4476183c62
Aidan Evans <38838579+nsnave@users.noreply.github.com>
2021-09-26 04:37:33 -0400

    changed "tests/one"

--- mclang/tests/one/one.ml
+++ mclang/tests/one/one.ml
@@ -8,15 +8,16 @@ open Lacamlext;;
 open Lacaml.Z;;
 
 let foobar() = (
-  let bases = Some(Qlib.Bases.z_basis, Qlib.Bases.x_basis) in
+  let _ = Some(Qlib.Bases.z_basis, Qlib.Bases.x_basis) in
   let p = (
     (*[Input(0, Zero); PrepList([1; 2])] @
     parse_pattern [J(0.0, 0, 1); J(0.0, 1, 2)];*)
-    [Input(0, Zero); Prep(1); Measure(0, 0.0, [], []); ZCorrect(1, [])]
+    (*[Input(0, Zero); Prep(1); Measure(0, 0.0, [], []); ZCorrect(1, [])]*)
+    [PrepList([0; 1]); Entangle(0, 1);  Measure(0, 0.0, [], []); XCorrect(1, [0])]
   ) in (
     let p' = standardize p in (
       print_prog p';
-      Mat.print (simulate ~change_base:bases p')
+      Mat.print (rand_eval ~shots:0 ~change_base:None p')
     )
   )
 );;
